e18d5331d7a106ace14897b99bf7190019f1edc2
EldrinGoki <eldrin.paul@gmail.com>
2016-01-17 00:43:30 +0800

    corrected some mistakes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,4 +26,4 @@ Route::get('test/rome2rio','ApiController@get_rome2rio');
 Route::post('test/foursquare','ApiController@post_foursquare');
 Route::post('test/rome2rio','ApiController@post_rome2rio');
 
-Route::get('test/api/dump', ['middleware' => 'cors', 'ApiController@testAjax']);
+Route::get('test/api/dump', ['middleware' => 'cors', 'uses' => 'ApiController@testAjax']);
