a08df945f682f83b5a376f4c8304470b2bf8f1bd
EldrinGoki <eldrin.paul@gmail.com>
2016-01-20 20:16:21 +0800

    commit

---
+++ database/migrations/2016_01_16_122837_create_segments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSegmentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('segments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('segment_origin');
+			$table->string('segment_destination');
+			$table->integer('price');
+			$table->integer('distance');
+			$table->integer('duration');
+			$table->enum('mode'), array('train','walk','ferry','plane','taxi'));
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('segments');
+	}
+
+}
