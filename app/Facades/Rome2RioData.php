35ad33e68dbac4ed7bdafa4254b005772edb184c
EldrinGoki <eldrin.paul@gmail.com>
2016-01-22 12:14:33 +0800

    asdasd

--- app/Facades/Rome2RioData.php
+++ app/Facades/Rome2RioData.php
@@ -11,11 +11,16 @@ use Illuminate\Support\Facades\Facade;
 
 class Rome2RioData extends Facade
 {	
-	protected $object;
+	protected $routes, $agencies, $aircrafts, $airlines, $airports, $places, $serveTime;
     //protected static function getFacadeAccessor() { return 'Rome2RioData'; }
 
 	public function __construct($object){
-		$this->object = $object;
+		$this->routes = $object->routes;
+		$this->agencies = $object->agencies;
+		$this->aircrafts = $object->aircrafts;
+		$this->airlines = $object->airlines;
+		$this->places = $object->places;
+		$this->serveTime = $object->serveTime;
 	}
 
     public static function getData(){
@@ -28,18 +33,37 @@ class Rome2RioData extends Facade
      */
     public function getRoutes($index = null){
     	
-    	if($index ! = null)
+    	
+    	if($index != null)
     	{
-    		return $data->routes['index'];
+    		return $this->routes[$index];
     	}
     	else
     	{
-    		return $data->routes;
+    		return $this->routes;
     	}
-
-    	
-
     }
+    /**
+     * Get Segments Function
+     * 
+     * @return Object
+     */
+    public function getSegments($index = null)
+    {
+    	if($index != null)
+    	{
+    		return $this->routes[$index];
+    	}
+    	else
+    	{
+    		return $this->routes;
+    	}
+    }
+
+    public function getStops()
+    {
+
+    }    
 }
 
 ?>
