ae9e1aaf2bcc91f5835866269436b9d931d4a14d
EldrinGoki <eldrin.paul@gmail.com>
2016-08-18 03:05:35 +0800

    small title change"

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -46,7 +46,7 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <div class="panel panel-default">
-                        <div class="panel-heading">Login</div>
+                        <div class="panel-heading">We'll find what your looking for</div>
                         <div class="panel-body">
                             @if (count($errors) > 0)
                                 <div class="alert alert-danger">
